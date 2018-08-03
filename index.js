2545f32b9c9ee0945038651c0136382926c4852a
Patrick Oberlohr <p.oberlohr@student.tugraz.at>
2018-08-03 04:08:20 +0200

    Setup NodeJS, NPM, ExpressJS and ESLint

---
+++ index.js
@@ -0,0 +1,12 @@
+// source: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/development_environment
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(8000, () => {
+    // console.log('Example app listening on port 8000!');
+});
