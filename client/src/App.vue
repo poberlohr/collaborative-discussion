bfde5becb9abd30a8198154db81af5477a986de5
Patrick Oberlohr <p.oberlohr@student.tugraz.at>
2018-08-09 00:34:49 +0200

    Create VueJS client project

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data () {
+            return {
+                message: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    #app {
+        font-family: Arial, sans-serif;
+    }
+</style>
--- public/index.js
+++
@@ -1,6 +0,0 @@
-const vue = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue! Create separate projects for client (vue) and api-server (express).'
-    }
-});
