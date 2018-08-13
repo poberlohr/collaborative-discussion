bb8c62783a39ed1efa391d7a0df90ecfa614aae9
Patrick Oberlohr <patrick.oberlohr@gmx.at>
2018-08-13 16:01:53 +0200

    Show claims from server

--- client/src/App.vue
+++ client/src/App.vue
@@ -26,48 +26,23 @@
     export default {
         name: 'app',
         data() {
-            return {
-                claims: [
-                    {
-                        title: 'Someone Should Create a Collaborative Open-Source Discussion Plattform',
-                        elaboration: 'Someone should create an online plattform, where people can discuss topics. The participants should not just exchange pros and cons, but rather argue pros and cons in collaboration. The plattform\'s source code should be available to the public for free.',
-                        pros: [
-                            {
-                                title: 'Open-Source Allows Instances for Specific Scopes of Application',
-                                elaboration: 'If the plattform is open-source, everyone can create a personal plattform suited to ones interests. E.g. one interested in "A Song of Ice and Fire" could create an instance with the sole purpose to discuss conspiracy theories regarding the world of ASOIAF.'
-                            },
-                            {
-                                title: 'Collaboration Improves Quality',
-                                elaboration: 'The more participants, the more knowledge, the higher quality.'
-                            }
-                        ],
-                        cons: [
-                            {
-                                title: 'Open Online Discussions don\'t Work due to Trolls',
-                                elaboration: 'It is entirely impossible to have a high-quality discussion where everyone is allowed to participate. Trolls don\'t participate, they destroy.'
-                            }
-                        ]
-                    },
-                    {
-                        title: 'A Collaborative Open-Source Discussion Plattform Should be Based on Claims',
-                        elaboration: 'The central entities of a collaborative open-source discussion plattform should be claims. People should start new topics by claiming something and should add claims to argue for or argainst something.'
-                    }
-                ]
-            }
+            return { claims: [] };
         },
         
         mounted() {
-            var requestOptions = { 
+            let requestOptions = {
                 method: 'GET',
                 mode: 'cors',
             };
 
-            var request = new Request('http://localhost:8000/claims', requestOptions);
+            let request = new Request('http://localhost:8000/claims', requestOptions);
             
             fetch(request).then((response) => {
-                console.log(response);
+                return response.json();
+            }).then((data) => {
+                this.claims = data.claims;
             }).catch((error) => {
-                console.log('error happened: ');
+                console.log('Error happened when fetching claims: ');
                 console.log(error);
             });
         }
