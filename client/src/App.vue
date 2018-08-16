edd356ce6aee90f7e6c4563644c33ee28083c8a2
Patrick Oberlohr <p.oberlohr@student.tugraz.at>
2018-08-17 00:33:42 +0200

    Add claim creation

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,20 @@
 <template>
     <div id="app">
         <h1>Let's Have an Argument!</h1>
+
+        <form v-if="isAddClaimFormShown" v-on:submit.prevent="saveNewClaim">
+            <label>
+                <span>Title:</span>
+                <input v-model="newClaim.title" type="text" />
+            </label>
+            <label>
+                <span>Elaborate:</span>
+                <textarea v-model="newClaim.elaboration" />
+            </label>
+            <button>Save</button>
+        </form>
+        <button v-else v-on:click="showAddClaimForm">Claim something new</button>
+
         <section v-for="claim in claims" class="claim">
             <section class="description">
                 <h2>{{ claim.title }}</h2>
@@ -26,9 +40,13 @@
     export default {
         name: 'app',
         data() {
-            return { claims: [] };
+            return {
+                claims: [],
+                isAddClaimFormShown: false,
+                newClaim: this.createEmptyNewClaim()
+            };
         },
-        
+
         mounted() {
             let requestOptions = {
                 method: 'GET',
@@ -36,7 +54,7 @@
             };
 
             let request = new Request('http://localhost:8000/claims', requestOptions);
-            
+
             fetch(request).then((response) => {
                 return response.json();
             }).then((data) => {
@@ -45,46 +63,117 @@
                 console.log('Error happened when fetching claims: ');
                 console.log(error);
             });
+        },
+
+        methods: {
+            createEmptyNewClaim() {
+                return {
+                    title: '',
+                    elaboration: ''
+                };
+            },
+
+            showAddClaimForm() {
+                this.isAddClaimFormShown = true;
+            },
+
+            saveNewClaim() {
+                fetch('http://localhost:8000/claims', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(this.newClaim),
+                    mode: 'cors'
+                }).then((response) => {
+                    console.log('post response:');
+                    console.log(response);
+
+                    if (response.status === 200 || response.status === 201) {
+                        this.claims.unshift(this.newClaim);
+                        this.newClaim = this.createEmptyNewClaim();
+                        this.isAddClaimFormShown = false;
+                    } else {
+                        const error = new Error(
+                            'Claim not saved: '
+                            + response.statusText
+                            + ' (' + response.status + ')');
+                        return Promise.reject(error);
+                    }
+                }).catch((error) => {
+                    alert(error);
+                    console.log('post error:');
+                    console.log(error);
+                });
+            }
         }
     }
 </script>
 
 <style lang="scss">
+    * {
+        box-sizing: border-box;
+    }
+
     body {
         background-color: #234;
     }
-    
+
     #app {
         width: 800px;
         margin: 0 auto;
         font-family: Arial, sans-serif;
     }
-    
+
     h1 {
         text-align: center;
         color: #e8f0ff;
     }
-    
+
     h2, h3, p { margin: 20px 0; }
-    
+
+    form, .claim {
+        color: #234;
+        background-color: #def;
+        border-radius: 20px;
+        margin: 20px 0;
+    }
+
+    form {
+        padding: 20px;
+    }
+
+    form > * {
+        margin: 20px 0;
+        display: block;
+    }
+
+    form > *:first-child { margin-top: 0; }
+
+    form > *:last-child { margin-bottom: 0; }
+
+    form label, form label > * {
+        display: block;
+        width: 100%;
+    }
+
     .claim {
         display: grid;
         grid-template-columns: 1fr 1fr;
         grid-template-areas:
             "header header"
             "pros cons";
-        
-        color: #234;
-        background-color: #def;
-        border-radius: 20px;
-        margin: 20px 0;
+
         padding: 0 20px;
         grid-column-gap: 10px;
     }
-    
+
     .claim > .description { grid-area: header; }
-    
+
     .claim > .pros { grid-area: pros; }
-    
+
     .claim > .cons { grid-area: cons; }
+
+
 </style>
--- server/routes/claimRouter.js
+++ server/routes/claimRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 const Claim = require('../models/claim.js');
 
@@ -16,6 +17,25 @@ router.get('/', (request, response) => {
 
             response.json({ claims: claims });
         });
+});
+
+router.post('/', (request, response) => {
+    console.log('Going to create claim: ' + request.body);
+    // TODO: Validate request
+    const claimToSave = request.body;
+    claimToSave._id = new mongoose.Types.ObjectId();
+    Claim.create(claimToSave, (error, savedClaim) => {
+        if (error) {
+            response.status(500).send({ error: error });
+        } else {
+            const location = request.protocol
+                + '://'
+                + request.get('host')
+                + request.originalUrl
+                + savedClaim.id;
+            response.status(201).append('Location', location).json(savedClaim);
+        }
+    });
 });
 
 module.exports = router;
